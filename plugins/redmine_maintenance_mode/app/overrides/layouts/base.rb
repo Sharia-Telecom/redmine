9bc7f4985d1ebe175adc7ebcc18129a22f369319
Sharia Founder <shariatelecom@gmail.com>
2017-08-30 08:02:48 +0700

    Upload redmine_maintenance_mode plugin

---
+++ plugins/redmine_maintenance_mode/app/overrides/layouts/base.rb
@@ -0,0 +1,5 @@
+Deface::Override.new :virtual_path  => 'layouts/base',
+                     :name          => 'add-maintenance-message-to-bodytop',
+                     :original      => 'b85b7e064a792d811d7060c206f55f08797aef91',
+                     :insert_top    => 'body',
+                     :partial       => 'maintenance/maintenance_body'
