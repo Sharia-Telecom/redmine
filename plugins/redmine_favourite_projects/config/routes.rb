138dfad12450fbdc0c6f28a124238cf7d9a28210
Sharia Founder <shariatelecom@gmail.com>
2017-09-17 16:34:33 +0700

    Delete plugins suspected in relation to my page 500 error

--- plugins/redmine_favourite_projects/config/routes.rb
+++
@@ -1,9 +0,0 @@
-if Rails::VERSION::MAJOR >= 3
-  RedmineApp::Application.routes.draw do
-    match 'favourite_projects/:action', :controller => 'favourite_projects', :via => [:get, :post]
-  end
-else
-  ActionController::Routing::Routes.draw do |map|
-    map.connect 'favourite_projects/:action', :controller => 'favourite_projects'
-  end
-end
